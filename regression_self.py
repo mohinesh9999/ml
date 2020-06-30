134b216fb3c095c335c2ed3661a3b91300aeeb6b
mohinesh9999 <mohineshsharma9999@gmail.com>
2020-06-30 21:33:34 +0530

    regression

---
+++ regression_self.py
@@ -0,0 +1,50 @@
+# m(slope)=(m(x).m(y)-m(x.y))/(m(x)**2-m(x**2))
+
+from statistics import mean
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import style
+
+import random #for testing
+style.use('fivethirtyeight')
+xs=np.array([1,2,3,4,5,6],dtype=np.float64)
+ys=np.array([5,4,6,5,6,7],dtype=np.float64)
+def create_dataset(hm,variance,step=2,correlation=False):
+    val=1
+    ys=[]
+    for i in range(hm):
+        y=val+random.randrange(-variance,variance)
+        ys.append(y)
+        if correlation and correlation=='pos':
+            val+=step
+        elif  correlation and correlation=='neg':
+            val-=step
+    xs=[i for i in range(len(ys))]
+    return np.array(xs,dtype=np.float64),np.array(ys,dtype=np.float64)
+def best_fit_slope_and_intercept(xs,ys):
+    m=( (mean(xs)*mean(ys)-mean(xs*ys)) /
+        (mean(xs)**2-mean(xs**2 )) )#PEMDAS
+    b=mean(ys)-m*mean(xs)
+    return m,b
+def squared_error(ys_orig,ys_line):
+    return sum((ys_line-ys_orig)**2)
+def coefficient_of_determination(ys_orig,ys_line):
+    y_mean_line=[mean(ys_orig) for u in ys_orig]
+    squared_error_regr=squared_error(ys_orig,ys_line)
+    squared_error_y_mean=squared_error(ys_orig,y_mean_line)
+    return 1-(squared_error_regr/squared_error_y_mean)
+
+xs,ys=create_dataset(40,10,2,correlation='pos')
+m,b=best_fit_slope_and_intercept(xs,ys)
+print(m,b)
+regression_line=m*xs+b
+rsuared=coefficient_of_determination(ys,regression_line)
+print(rsuared,squared_error(regression_line,ys))
+predict_x=8
+predict_y=(m*predict_x)+b
+
+
+plt.scatter(xs,ys)
+plt.scatter(predict_x,predict_y,color='green')
+plt.plot(xs,regression_line)
+plt.show()
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+st_fit_slope_and_intercept(xs,ys)
+# print
